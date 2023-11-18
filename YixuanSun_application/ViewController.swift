1a6c940ed5b718bff3c3ca22140afb9050dcb962
yixuansun <sun.yixu@northeastern.edu>
2023-11-17 19:13:59 -0800

    submit prewor

---
+++ YixuanSun_application/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  YixuanSun_application
+//
+//  Created by 鹿小葵 on 2023/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+       
+    }
+    
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+}
+
